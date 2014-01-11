1af028c6e544dc4d1c5936493f5c0ef552b3ae89
Vadim Zaliva <lord@crocodile.org>
2014-01-11 15:31:24 -0800

    test python script to test reading

---
+++ Arduino/SerialReader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# requires PySerial. On MacOS (port install py-serial)
+
+DEV='/dev/tty.usbmodem1411'
+
+import serial
+import sys
+
+s = serial.serialposix.Serial(port=DEV,
+                                      baudrate=9600, bytesize=8, parity='N', stopbits=1,
+                                      timeout=120,
+                                      rtscts=1)
+while True:
+    txt = s.readline()
+    sys.stdout.write(txt)
+
