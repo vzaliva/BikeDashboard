23b414da06279bec97e45a0457e5ab5458bf303c
Vadim Zaliva <lord@crocodile.org>
2014-01-27 22:27:45 -0800

    more nano experiments

--- Arduino/monitor_pin/monitor_pin_nano.ino
+++ Arduino/monitor_pin/monitor_pin_nano.ino
@@ -1,42 +1,29 @@
-// Definition of interrupt names
-#include < avr/io.h >
-// ISR interrupt service routine
-#include < avr/interrupt.h >
 
-volatile int last = 0;
 volatile unsigned int serial=0;
 
 // This is the INT0 Pin of the ATmega328 (Arduino Nano 3.x)
 int sensePin = 3;
+int ledPin = 13;
 
 // Install the interrupt routine.
-ISR(INT0_vect) {  
-  int value = digitalRead(sensePin);
-  if(value!=last)
-  {
-    if (value) {
-      Serial.println(serial);
-      serial++;
-    }
-  }
-  last = value;
+void handler() {
+  Serial.println(serial);
+  serial++;
 }
-
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Initializing ihandler");
   pinMode(sensePin, INPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.println("Processing initialization");
-  // Global Enable INT0 interrupt
-  EIMSK |= ( 1 << INT0);
-  // Signal change triggers interrupt
-  EICRA |= ( 1 << ISC00);
-  EICRA |= ( 0 << ISC01);
+  attachInterrupt(0, handler, CHANGE);
   Serial.println("Finished initialization");
 }
 
 void loop() {
-  //delay(100);    
+  //delay(100);
+  //digitalWrite(ledPin, serial & 1);
+
 }
 
